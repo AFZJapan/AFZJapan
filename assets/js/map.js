a1a764f3c09e93c5e79544472238d972ebebe1bb
akibaforever <166113687+akibaforever@users.noreply.github.com>
2025-04-23 19:18:52 +0900

    add popup

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,14 @@
 epsg4326 = new OpenLayers.Projection("EPSG:4326")
+var options = {
+        projection: new OpenLayers.Projection("EPSG:900913"),
+        displayProjection: epsg4326,
+        units: "m",
+        numZoomLevels: 19,
+        maxResolution: 156543.0339,
+        maxExtent: new OpenLayers.Bounds(-0.13011, -0.13011, 51.51039, 51.51039)
+    };
 
-map = new OpenLayers.Map("afz");
+map = new OpenLayers.Map("afz", options);
 map.addLayer(new OpenLayers.Layer.OSM("AFZ",
     ["https://tile.openstreetmap.jp/${z}/${x}/${y}.png"], {
         attribution: "&copy; <a href='https://www.openstreetmap.org/'>AFZ日本</a> ",
@@ -8,6 +16,8 @@ map.addLayer(new OpenLayers.Layer.OSM("AFZ",
     }));
 
 map.addControls([
+    new OpenLayers.Control.DragPan(),
+    new OpenLayers.Control.ScaleLine(),
     new OpenLayers.Control.LayerSwitcher(),
     new OpenLayers.Control.Permalink({ anchor: true })
 ]);
@@ -19,45 +29,67 @@ if (!map.getCenter()) {
     map.setCenter(lonLat, zoom);
 }
 
-// point-definitions
-var markers = [
-    [135.78231488193407, 35.039196850218715, 'お店・地元企業', 'shop'], // お店 6
-    [134.74649203721995, 34.336122136273445, 'お店・地元企業', 'shop'], // お店 7
-    [139.5283068200659, 35.70170448084507, 'お店・地元企業', 'shop'], // お店 8
-    [139.73252432225615, 35.70422686480873, '飲食店', 'cafe'], // 飲食店 9
-    [139.64859456870067, 35.70543004185311, '飲食店', 'cafe'], // 飲食店 10
-    [132.7155055110331, 33.86380191240722, 'お店・地元企業', 'shop'], // お店 12
-    [139.74590332445408, 35.73898748822561, '飲食店', 'cafe'], // 飲食店 3
-    [141.31954732309313, 43.04903947510028, 'ソーシャル・社交の場', 'social'], // 合同会社エクレシア
-    [139.64632521047002, 35.70674622831693, 'ソーシャル・社交の場', 'social'], // NamNam
-];
+var iconSize = new OpenLayers.Size(24, 24);
+var iconOffset = new OpenLayers.Pixel(-(iconSize.w / 2), -iconSize.h);
+var zoom, center, currentPopup, map;
+var layers = [];
+var popupClass = OpenLayers.Class(OpenLayers.Popup.FramedCloud, {
+    "autoSize": true,
+    "minSize": new OpenLayers.Size(200, 50),
+    "maxSize": new OpenLayers.Size(400, 300),
+    "keepInMap": true
+});
+var bounds = new OpenLayers.Bounds();
+var icons = [];
+const categories = {
+    social: "ソーシャル・社交の場",
+    cultural: "文化的な場所",
+    cafe: "飲食店",
+    shop: "お店・地元企業",
+    public: "公的機関"
+};
+var i = 0;
+for (var key in categories) {
+    icons.push(new OpenLayers.Icon("assets/icons/" + key + ".png", iconSize, iconOffset));
+    layers.push(new OpenLayers.Layer.Markers(categories[key]));
+    map.addLayer(layers[i]);
+    i++;
+}
 
-var layerName = [markers[0][2]];
-var styleArray = [new OpenLayers.StyleMap({ externalGraphic: 'assets/icons/' + [markers[0][3]] + '.png', graphicWidth: 24, graphicHeight: 24, graphicYOffset: -24, title: [markers[0][2]] })];
-var vectorLayer = [new OpenLayers.Layer.Vector(layerName[0], { styleMap: styleArray[0] })];
+function addMarker(lon, lat, icon, info) {
+    var pt = new OpenLayers.LonLat(lon, lat).transform(epsg4326, projectTo);
+    bounds.extend(pt);
+    var index = Object.keys(categories).indexOf(icon);
+    var feature = new OpenLayers.Feature(layers[index], pt);
+    feature.closeBox = true;
+    feature.popupClass = popupClass;
+    feature.data.popupContentHTML = info;
+    feature.data.overflow = "auto";
+    var marker = new OpenLayers.Marker(pt, icons[index].clone());
+    var markerClick = function(evt) {
+        if (currentPopup != null && currentPopup.visible()) {
+            currentPopup.hide();
+        }
+        if (this.popup == null) {
+            this.popup = this.createPopup(this.closeBox);
+            map.addPopup(this.popup);
+            this.popup.show();
+        } else {
+            this.popup.toggle();
+        }
+        currentPopup = this.popup;
+        OpenLayers.Event.stop(evt);
+    };
+    marker.events.register("mousedown", feature, markerClick);
+    layers[index].addMarker(marker);
+}
 
-var j = 0;
-for (var i = 1; i < markers.length; i++) {
-    if (!layerName.includes(markers[i][2])) {
-        j++;
-        layerName.push(markers[i][2]);
-        styleArray.push(new OpenLayers.StyleMap({ externalGraphic: 'assets/icons/' + [markers[i][3]] + '.png', graphicWidth: 24, graphicHeight: 24, graphicYOffset: -24, title: '' + [markers[j][2]] }));
-        vectorLayer.push(new OpenLayers.Layer.Vector(layerName[j], { styleMap: styleArray[j] }));
+fetch('/data/map.json')
+    .then((response) => response.json())
+    .then((json) => display(json));
+
+function display(json) {
+    for (var i = 0; i < json.length; i++) {
+        addMarker(json[i][0], json[i][1], json[i][2], json[i][3]);
     }
-}
-
-// Loop through the markers array
-for (var i = 0; i < markers.length; i++) {
-    var lon = markers[i][0];
-    var lat = markers[i][1];
-    var feature = new OpenLayers.Feature.Vector(
-        new OpenLayers.Geometry.Point(lon, lat).transform(epsg4326, projectTo),
-            { description: markers[i][2] }
-            // see http://dev.openlayers.org/docs/files/OpenLayers/Feature/Vector-js.html#OpenLayers.Feature.Vector.Constants for more options
-    );
-    vectorLayer[layerName.indexOf(markers[i][2])].addFeatures(feature);
-}
-
-for (var i = 0; i < layerName.length; i++) {
-    map.addLayer(vectorLayer[i]);
 }
