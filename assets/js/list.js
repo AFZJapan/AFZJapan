e4c39bb6d933c2e84726d185548721e0044b8bbc
akibaforever <166113687+akibaforever@users.noreply.github.com>
2025-07-07 20:13:01 +0900

    fix icons in data/list

--- assets/js/list.js
+++ assets/js/list.js
@@ -14,7 +14,7 @@ let displayList = () => {
         let afz = result.afzs;
         for (let i = 0; i < afz.length; i++) {
             rows += `<tr>
-                       <td style="vertical-align: bottom;">${afz[i].name} <br/>`;
+                       <td style="vertical-align: bottom;">${afz[i].name} <img align='top' src='/assets/icons/${afz[i].icon}.png' width='20px' height='20px' /><br/>`;
             for (const key in afz[i].tags) {
             rows += `<div class="chip outlined" style="vertical-align: middle; font-size: 10px; height: 24px; line-height:24px; margin-bottom:0px">${afz[i].tags[key]}</div>`;
             }
@@ -41,7 +41,7 @@ let filterByPlace = () => {
         for (let i = 0; i < afz.length; i++) {
             if (afz[i].place.includes(val)) {
                 rows += `<tr>
-                           <td>${afz[i].name}</td>
+                           <td style="vertical-align: bottom;">${afz[i].name} <img align='top' src='/assets/icons/${afz[i].icon}.png' width='20px' height='20px' /><br/>
                            <td>${afz[i].place}</td>
                            <td>${afz[i].sns}</td>
                          </tr>`;
