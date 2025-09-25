44d9e4ae8628f88dd6bd5ae75aabf8848b69f421
akibaforever <166113687+akibaforever@users.noreply.github.com>
2025-09-26 02:43:49 +0900

    add FilmLab icon

--- assets/js/list.js
+++ assets/js/list.js
@@ -14,7 +14,7 @@ let displayList = () => {
         let afz = result.afzs;
         for (let i = 0; i < afz.length; i++) {
             rows += `<tr>
-                       <td style="vertical-align: bottom;">${afz[i].name} <img align='top' src='/assets/icons/${afz[i].icon}.png' width='20px' height='20px' /><br/>`;
+                       <td style="vertical-align: bottom;">${afz[i].name} <img align='top' src='/assets/icons/${afz[i].icon}.png' width='20px' height='20px' /> `; if (afz[i].cinema) { rows += ` <img align='top' src='/assets/icons/cinema_small.png' width='20px' height='20px' /> ` } rows += ` <br/>`;
             for (const key in afz[i].tags) {
             rows += `<div class="chip outlined" style="vertical-align: middle; font-size: 10px; height: 24px; line-height:24px; margin-bottom:0px">${afz[i].tags[key]}</div>`;
             }
