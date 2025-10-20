1474dac033003a90a16294ed513105d74ecc14ce
chikaku <eidantoei@gmail.com>
2025-10-20 21:51:07 +0900

    wip: cinemadays2025: 上映会場のウェブサイトへのリンクを表示可能にする

--- assets/js/list.cinema.js
+++ assets/js/list.cinema.js
@@ -14,7 +14,10 @@ let displayList = () => {
         let afz = result.afzs;
         for (let i = 0; i < afz.length; i++) {
             rows += `<tr>
-                       <td style="vertical-align: bottom;">${afz[i][3]} <br/>`;
+                       <td style="vertical-align: bottom;">${afz[i][3]}`;
+            if (afz[i][4]) {
+                rows += ` <a href="${afz[i][4]}" target="_blank"><img src="/assets/icons/website.png" width="16" height="16"></a>`;
+            }
             rows += `
                        </td>
                        <td style="word-break: keep-all;">${afz[i][2]}</td>
